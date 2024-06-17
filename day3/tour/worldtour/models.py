fc78907d822a81aac4120cdc1f3ab3f9332f2a81
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-17 11:46:58 +0500

    Add: added day 3, learned about createing, models, shell , to create crud on this model

---
+++ day3/tour/worldtour/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=64)
+    destination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.origin_country} to {self.destination_country}"
+    
+    
+
+    
