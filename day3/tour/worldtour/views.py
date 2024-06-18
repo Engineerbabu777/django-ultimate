ddc97df2e2d4f23df853eb14a7bddda87c685f18
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-18 06:02:28 +0500

    add: displayuing, adding data from db in templates

--- day3/tour/worldtour/views.py
+++ day3/tour/worldtour/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Tour
 
 # Create your views here.
+def index(request):
+    tours = Tour.objects.all()
+    return render(request,'index.html',{'tours':tours})
+
