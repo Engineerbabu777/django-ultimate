08836b096a05024bcf056a69a9a1858d0634bcd2
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-21 11:16:26 +0500

    add: project setup, apps added, contatct vuew and honme view creatred

---
+++ forms/forms/form_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from .form import ContactForm
+
+
+
+# Create your views here.
+def home_view(request):
+    return render(request,'home.html')
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            # form.save()
+            return redirect('contact')
+            # return render(request,'contact.html',{'form':ContactForm()})
+    else:
+        form = ContactForm()
+    
+    return render(request,'contact.html',{'form':form})
